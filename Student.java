e86a25149c8501793685ebe23dce9a355aa332e6
Rujan <np03cs4a230295@heraldcollege.edu.np>
2025-01-12 14:31:48 +0545

    Added getter methods to Student class

--- Student.java
+++ Student.java
@@ -1,11 +1,20 @@
 package Week8;
 
 public class Student {
-	String name;
-	int age;
+	private String name;
+	private int age;
 	
 	Student(String name,int age){
 		this.name = name;
 		this.age = age;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
 }
